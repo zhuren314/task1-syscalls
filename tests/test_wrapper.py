26319ca77d6f8e4635818f754dc9f7ad3b5cc211
Redha Gouicem <redha.gouicem@gmail.com>
2021-04-07 18:52:15 +0200

    tests: fix

--- tests/test_wrapper.py
+++ tests/test_wrapper.py
@@ -7,22 +7,25 @@ from testsupport import run, subtest
 
 def main() -> None:
 
+    # Get test abspath
+    HERE = os.path.dirname(os.path.abspath(sys.argv[0]))
+
     # Generate a small random file
-    with open("rdn.txt", "wb") as fp:
+    with open(f"{HERE}/rdn.txt", "wb") as fp:
         fp.write(os.urandom(4096))
 
     # Copy the file with the system's glibc
     with subtest("Run cp with system glibc"):
-        run([ "cp", "tests/rdn.txt", "tests/rdn.glibc.txt" ])
+        run([ "cp", f"{HERE}/rdn.txt", f"{HERE}/rdn.glibc.txt" ])
 
     # Copy files with LD_PRELOADed librw.so.1
     with subtest("Run cp with librw.so.1 preloaded"):
-        run([ "cp", "tests/rdn.txt", "tests/rdn.librw.1.txt" ],
-            extra_env={"LD_PRELOAD": os.getcwd()+"/../librw.so.1"})
+        run([ "cp", f"{HERE}/rdn.txt", f"{HERE}/rdn.librw.1.txt" ],
+            extra_env={"LD_PRELOAD": f"{HERE}/../librw.so.1"})
 
     # Check that glibc and librw.so.1 give the same result
     with subtest("Check that both resulting files are identical"):
-        run([ "cmp", "tests/rdn.glibc.txt", "tests/rdn.librw.1.txt" ])
+        run([ "cmp", f"{HERE}/rdn.glibc.txt", f"{HERE}/rdn.librw.1.txt" ])
 
     # # Check that the nbytes == 0 optimisation is implemented
     # with subtest("Check that librw.so.1 avoids the syscall if nothing will be read/written"):
